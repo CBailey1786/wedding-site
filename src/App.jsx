448395e1a1a834ec2fb0df3883e58e5444aed400
Cameron Bailey <cameron.bailey25@yahoo.co.uk>
2025-09-26 16:27:10 +0100

    Updated names as test

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import './App.css'
 function App() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center text-center px-6">
-      <h1 className="text-4xl font-bold mb-4">Emma & James</h1>
+      <h1 className="text-4xl font-bold mb-4">Amanda & Cameron</h1>
       <p className="text-xl mb-2">Save the Date</p>
-      <p className="text-2xl font-semibold mb-6">May 10, 2026</p>
+      <p className="text-2xl font-semibold mb-6">13 June 2026</p>
       <p className="text-md">Formal invitation to follow</p>
     </div>
   );
